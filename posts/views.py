97a63d834c4e69b31a5178edc61caad2b2a6cd94
Juan Duran <juandb182@gmail.com>
2021-03-11 16:00:00 -0400

    data modificated and view feed fixed

--- posts/views.py
+++ posts/views.py
@@ -4,8 +4,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from datetime import datetime
+
+
+
+posts = [
+    {
+        'title':'Mont blanc',
+        'user': {
+            'name':'Jessica Cortez',
+            'picture':'https://pbs.twimg.com/media/CqS2i-nXYAAl5d9?format=jpg&name=large'
+        },
+        'media': 'https://pbs.twimg.com/media/Cwl1CbcXAAANPU1?format=jpg&name=large',
+        'description': 'a star war pixel arte incredible',
+        'timestamp':datetime.now().strftime('%b %dth %Y - %H:%M Hrs'),
+    },
+    {
+        'title':'Lex ipsum',
+        'user': {
+            'name':'Octo Hatred',
+            'picture':'https://pbs.twimg.com/media/Cpx4mhTWYAAauKc?format=jpg&name=large'
+        },
+        'media':'https://pbs.twimg.com/media/CqS2i-nXYAAl5d9?format=jpg&name=large',
+        'description':'Different photo today',
+        'timestamp':datetime.now().strftime('%b %dth %Y - %H:%M Hrs'),
+    },
+     {
+        'title':'Ardu Ino',
+        'user': {
+            'name':'Dert Exinos',
+            'picture':'https://pbs.twimg.com/media/Cwl1CbcXAAANPU1?format=jpg&name=large'
+        },
+        'media':'https://pbs.twimg.com/media/Cpx4mhTWYAAauKc?format=jpg&name=large',
+        'description':'Bep Bep Robot',
+        'timestamp':datetime.now().strftime('%b %dth %Y - %H:%M Hrs'),
+    },
+]
 
 # Create your views here.
 def list_posts(request):
-    posts = [1,4,5]
-    return HttpResponse(str(posts))
+    
+    return render(request,'feed.html',{'posts':posts})
+  
