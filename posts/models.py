a74421a87180c6f0c7d5a480b42ca8fdc8ad0765
Juan Duran <juandb182@gmail.com>
2021-03-11 22:02:01 -0400

    added a user model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True,null=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.email
